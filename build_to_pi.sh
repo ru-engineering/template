4831e10d0530b95ef9b581e65fb38225ccf60852
KjartanKlein <kjartanbklein@gmail.com>
2020-08-30 16:37:20 +0000

    Modified the build_to_pi script so NOT crosscompiling is ez

--- build_to_pi.sh
+++ build_to_pi.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # EDIT THEESE PARAMETERS TO FIT YOUR NEED
-pis_ip="192.168.137.12"
+pis_ip="192.168.137.202"
 pis_pass=password #THIS IS FOR THE PASSWORD FOR THE PI
 pis_user=pi #THIS IS THE USERNAME ON THE PI
 pis_path=/home/$pis_user #THIS IS THE DIRECTORY THAT THE PROGRAM WILL BE DROPPED IN
@@ -9,13 +9,13 @@ program_name=rust_template #THIS IS THE NAME OF THE RUST PROGRAM FOUND IN DEBUG
 
 
 # scrip recuires sshpass, run sudo apt-get install sshpass
-
-if cargo build; then
+echo "attemting to build program $program_name and moving it to $pis_ip:$pis_path"
+if cargo build --target="arm-unknown-linux-gnueabihf"; then
 	echo "Build succesfull, sending file over"
 	if sshpass -p $pis_pass scp target/arm-unknown-linux-gnueabihf/debug/$program_name $pis_user@$pis_ip:$pis_path; then
 		echo "Program uploaded, running"
 		echo "------------------------------------------"
-		if sshpass -p $pis_pass ssh $pis_user@$pis_ip $pis_path/$program_name; then
+		if sshpass -p $pis_pass ssh -x -t $pis_user@$pis_ip $pis_path/$program_name; then
 		echo "------------------------------------------"
 		echo "Program stopped"
 		fi
